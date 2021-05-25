2d54757b8c0a0fe5e1b1e92c929c60798b320c4c
JUNG\green <green5940@gmail.com>
2021-05-26 08:29:22 +0900

    refact: mongoose transform 변경  예시와 동일하게 표현되도록 수정

--- src/models/metadata.ts
+++ src/models/metadata.ts
@@ -18,12 +18,19 @@ const Metadata = new mongoose.Schema(
   {
     toObject: {
       transform: function (doc, ret) {
-        delete ret._id;
-        delete ret.__v;
-        delete ret.createdAt;
-        delete ret.updatedAt;
+        // delete ret._id;
+        // delete ret.__v;
+        // delete ret.createdAt;
+        // delete ret.updatedAt;
+
+        return {'date': doc.updatedAt,
+        'description' : doc.description,
+        'image' : doc.image,
+        'publisher' : doc.publisher,
+        'title' : doc.title,
+        'url' : doc.url}
       }
-    }, timestamps: { updatedAt: 'date' }
+    }, timestamps: true
   },
 );
 
