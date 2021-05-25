4877748238d9cc669d0e362a3c98a258f583995b
JUNG\green <green5940@gmail.com>
2021-05-25 13:48:44 +0900

    최초 서버 구동 가능한 상태

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ async function startServer() {
      * Well, at least in node 10 without babel and at the time of writing
      * So we are using good old require.
      **/
-    await require('.loaders').default({ expressApp: app });
+    await require('./loaders').default({ expressApp: app });
 
     app.listen(config.port, () => {
         Logger.info(`
